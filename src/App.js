e022afd33b1fe5b0ccfaf9f3a2440a24bb4f0351
AbhirupDey12 <abhirupd470@gmail.com>
2023-01-21 10:34:03 +0530

    UseMemo Learnt

--- src/App.js
+++ src/App.js
@@ -1,74 +1,23 @@
-import React from 'react';
-import Table from 'react-bootstrap/Table';
+import React from 'react'
 
 export default function App() {
 
-  const users = [
-    {
-      name: 'anil', email: "anil@test.com", address: [
-        { Hn: "10", city: "Noida", country: "India" },
-        { Hn: "11", city: "Delhi", country: "India" },
-        { Hn: "34", city: "Kolkata", country: "India" },
-        { Hn: "100", city: "Chennai", country: "India" },
-      ]
-    },
-    {
-      name: 'sidhu', email: "sidhu@test.com", address: [
-        { Hn: "10", city: "Noida", country: "India" },
-        { Hn: "11", city: "Delhi", country: "India" },
-        { Hn: "34", city: "Kolkata", country: "India" },
-        { Hn: "100", city: "Chennai", country: "India" },
-      ]
-    },
-    {
-      name: 'sidhu', email: "sidhu@test.com", address: [
-        { Hn: "10", city: "Noida", country: "India" },
-        { Hn: "11", city: "Delhi", country: "India" },
-        { Hn: "34", city: "Kolkata", country: "India" },
-        { Hn: "100", city: "Chennai", country: "India" },
-      ]
-    },
-    {
-      name: 'sidhu', email: "sidhu@test.com", address: [
-        { Hn: "10", city: "Noida", country: "India" },
-        { Hn: "11", city: "Delhi", country: "India" },
-        { Hn: "34", city: "Kolkata", country: "India" },
-        { Hn: "100", city: "Chennai", country: "India" },
-      ]
-    }
-  ]
+  const [count , setCount] = React.useState(0) ;
+  const [item , setItem] = React.useState(10) ;
+
+  const multiCountMemo = React.useMemo(function multiCount() {
+    console.warn('multiCount');
+    return count * 5 ;
+  } , [count]) ;
 
   return (
-
     <div>
-      <h1>Nested List</h1>
-      <Table striped bordered hover>
-        <tbody>
-          <tr>
-            <td>Name</td>
-            <td>Email</td>
-            <td>Address</td>
-          </tr>
-        </tbody>
-        {
-          users.map((item) =>
-            <tr>
-              <td>{item.name}</td>
-              <td>{item.email}</td>
-              {
-                item.address.map((data) =>
-                  <tr>
-                    <td>{data.Hn}</td>
-                    <td>{data.city}</td>
-                    <td>{data.country}</td>
-                  </tr>
-                )
-              }
-            </tr>
-          )
-        }
-      </Table>
+      <h1>Use Memo Hook React</h1>
+      <h2> Count : {count} </h2>
+      <h2> Item : {item} </h2>
+      <h2>{multiCountMemo}</h2>
+      <button onClick={() => setCount(count + 1) } >Update Count</button>
+      <button onClick={() => setItem(item * 10) } >Update Item</button>
     </div>
   )
-
 }
--- src/User.js
+++ src/User.js
@@ -1,17 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react'
 
-export default function User(props) {
-
-     useEffect(() => {
-          alert("count is " + props.count );
-     } , [props.count , props.data ]);
-
-     return (
-          <div>
-               <h1> Count Props : {props.count} </h1>
-               <h1> Data Props : {props.data} </h1>
-          </div>
-     )
-
+export default function User() {
+  return (
+    <div>User</div>
+  )
 }
-
